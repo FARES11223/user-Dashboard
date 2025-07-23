c50a49f7ebfe04c361c0235f182f1cef748b3094
unknown <fm3607477@gmail.com>
2025-07-23 14:11:00 +0300

    for deploy

--- config/redisConnection.js
+++ config/redisConnection.js
@@ -1,20 +1,20 @@
-const { createClient } = require("redis");
+// const { createClient } = require("redis");
 
-async function connectToRedis() {
-  try {
-    const redisClient = createClient({
-      url: process.env.REDIS_URL, // Redis server URL (Memurai)
-    });
+// async function connectToRedis() {
+//   try {
+//     const redisClient = createClient({
+//       url: process.env.REDIS_URL, // Redis server URL (Memurai)
+//     });
 
-    redisClient.on("error", (err) => {
-      console.error("Redis connection error:", err);
-    });
+//     redisClient.on("error", (err) => {
+//       console.error("Redis connection error:", err);
+//     });
 
-    await redisClient.connect();
-    console.log("Connected to Redis");
-  } catch (error) {
-    console.error("Failed to connect to Redis:", error);
-  }
-}
+//     await redisClient.connect();
+//     console.log("Connected to Redis");
+//   } catch (error) {
+//     console.error("Failed to connect to Redis:", error);
+//   }
+// }
 
-module.exports = connectToRedis;
+// module.exports = connectToRedis;
